1410c8a131422a13438bc69c8476e6e4e86a68cf
xd-okay <aldiyar.djumadil@gmail.com>
2026-02-05 21:14:52 +0500

    Prac 2

---
+++ Practice1/python-basics/if_else.py
@@ -0,0 +1,14 @@
+age = 16
+
+if age < 18:
+  pass # TODO: Add underage logic later
+else:
+  print("Access granted")
+  
+  
+  
+  
+# a = 10
+# b = 20
+# bigger = a if a > b else b
+# print("Bigger is", bigger)
